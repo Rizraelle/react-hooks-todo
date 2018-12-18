7fa600f9d81240894779b950cacf24dc96d0feb3
Rizraelle <rizraelleshinobu@gmail.com>
2018-12-18 19:25:49 +0300

    solve problem with deleting todo

---
+++ src/useTodoState.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+const nanoid = require('nanoid');
+
+export default initialValue => {
+  const [todos, setTodos] = useState(initialValue);
+
+  return {
+    todos,
+    addTodo: todoText => {
+      setTodos([...todos, { todoText, id: `item ${nanoid()}` }]);
+    },
+    deleteTodo: todoIndex => {
+      const newTodos = todos
+        .filter((_, index) => index !== todoIndex);
+      setTodos(newTodos);
+    }
+  }
+}
